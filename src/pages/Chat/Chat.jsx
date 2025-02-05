fc7870559b7d3909e1d7e7c969f2d345d157d84b
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-06 01:26:50 +0530

    Your commit message here

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import "./Chat.css"
+import React from "react";
+import "./Chat.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar.jsx";
+import ChatBox from "../../components/ChatBox/ChatBox.jsx";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
