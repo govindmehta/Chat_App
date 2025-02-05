5b9b56a8d43f82030af1a21e84073f46784c30ad
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-06 00:08:07 +0530

    Basic pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes ,Route } from 'react-router-dom'
+import Login from "./pages/Login/Login.jsx"
+import Chat from "./pages/Chat/Chat.jsx"
+import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate.jsx"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/chat' element={<Chat/>}/>
+      <Route path='/profile' element={<ProfileUpdate/>}/>
+    </Routes>
+    </>
   )
 }
 
