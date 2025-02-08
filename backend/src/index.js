e3362df06d0317452f346615e3169a6958c5bc27
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-08 10:45:43 +0530

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,10 @@ import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express();
+app.use(cors());
 app.use(cookieParser())
 app.use(express.json())
 
