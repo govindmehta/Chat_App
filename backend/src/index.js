85acb2dc4def424104628392f25cb2f9c0cc3865
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-08 10:38:04 +0530

    backend complete without socketio

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
@@ -12,6 +13,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 connectDB().then(() => {
     app.listen(PORT, () => {
