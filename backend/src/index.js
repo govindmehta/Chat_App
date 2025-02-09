cdfd419fc4875134fb0123e1b3f6c9348760b54c
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-09 17:04:15 +0530

    set up socket sever

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,33 @@
-import express from "express"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import dotenv from "dotenv"
-import {connectDB} from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true,                 
-}));
-app.use(cookieParser())
-app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
 
 dotenv.config();
 const PORT = process.env.PORT;
 
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
       console.log(`Server is running on PORT: ${PORT}`);
     });
-  }).catch(err => {
+  })
+  .catch((err) => {
     console.error("Database connection failed:", err);
   });
